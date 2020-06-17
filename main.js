8747086462da3e8782d9a5cd0c50fe8e7aa05993
Ammar <ammaralammalik221@gmail.com>
2020-06-17 18:46:20 +0530

    gallery and plus icon

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-console.log("asdasd");
+//console.log("asdasd");
 
 function fitElementToParent(el, padding) {
   var timeout = null;
@@ -95,3 +95,27 @@ var sphereAnimation = (function () {
 
   init();
 })();
+
+function currentDiv(n) {
+  showDivs((slideIndex = n));
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  if (n > x.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = x.length;
+  }
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" w3-opacity-off", "");
+  }
+  x[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " w3-opacity-off";
+}
